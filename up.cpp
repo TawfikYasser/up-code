076b176cb8c0ca969a524405c48454ec23d93b46
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-02-23 10:23:21 +0200

    Update up.cpp

--- up.cpp
+++ up.cpp
@@ -2,8 +2,9 @@
 #include<ctime>
 #include<time.h>
 #include <iomanip>
+#include<unordered_map>
+#include<algorithm>
 using namespace std;
-const int ARRAY_SIZE = 4;
 class Day
 {
 public:
@@ -11,6 +12,56 @@ public:
     int open, high, low, close;
     float avg;
 };
+const int ARRAY_SIZE = 4;
+unordered_map<string, int> monthsMap;
+
+// Function which initializes the monthsMap
+void sort_months()
+{
+    monthsMap["Jan"] = 1;
+    monthsMap["Feb"] = 2;
+    monthsMap["Mar"] = 3;
+    monthsMap["Apr"] = 4;
+    monthsMap["May"] = 5;
+    monthsMap["Jun"] = 6;
+    monthsMap["Jul"] = 7;
+    monthsMap["Aug"] = 8;
+    monthsMap["Sep"] = 9;
+    monthsMap["Oct"] = 10;
+    monthsMap["Nov"] = 11;
+    monthsMap["Dec"] = 12;
+}
+
+bool comp(Day a, Day b)
+{
+    // Comparing the years
+    string str1 = a.date.substr(7, 5);
+    string str2 = b.date.substr(7, 5);
+    if (str1.compare(str2) != 0) {
+        if (str1.compare(str2) > 0)
+            return true;
+        return false;
+    }
+
+    // Comparing the months
+    string month_sub_a = a.date.substr(3, 3);
+    string month_sub_b = b.date.substr(3, 3);
+
+    // Taking numeric value of months from monthsMap
+    int month_a = monthsMap[month_sub_a];
+    int month_b = monthsMap[month_sub_b];
+    if (month_a != month_b) {
+        return month_a > month_b;
+    }
+
+    // Comparing the days
+    string day_a = a.date.substr(0, 2);
+    string day_b = b.date.substr(0, 2);
+    if (day_a.compare(day_b) > 0)
+        return true;
+    return false;
+}
+
 string todayDate()
 {
     time_t t ;
@@ -49,13 +100,16 @@ void defaultData(Day arr_days[])
     arr_days[1].close = 10;
     arr_days[1].avg = 0;
     //Day 3
-    arr_days[2].date = "28 Feb 2021";
+    arr_days[2].date = "28 Jan 2021";
     arr_days[2].open = 100;
     arr_days[2].high = 100;
     arr_days[2].low = 100;
     arr_days[2].close = 20;
     arr_days[2].avg = 0;
 }
+
+
+
 void calculateAVGClosingPrice(Day arr_days[])
 {
     int sum = 0;
@@ -67,6 +121,7 @@ void calculateAVGClosingPrice(Day arr_days[])
 }
 int main()
 {
+    cout<<todayDate()<<endl;
     // Array with 4 days, each day has 6 parts [Date - Open - High - Low - Close - AVG]
     Day arr_days[ARRAY_SIZE];
     defaultData(arr_days);
@@ -87,6 +142,15 @@ int main()
         cout<<arr_days[i].date<<" - "<<arr_days[i].open<<"   - "<<arr_days[i].high<<"   - "
             <<arr_days[i].low<<"  - "<<arr_days[i].close<<" - "<<setw(10)<<arr_days[i].avg<<endl;
     }
+
+    sort_months();
+
+    sort(arr_days, arr_days + ARRAY_SIZE, comp);
     cout<<"After sorting"<<endl;
+        for(int i = 0 ; i<ARRAY_SIZE; i++)
+    {
+        cout<<arr_days[i].date<<" - "<<arr_days[i].open<<"   - "<<arr_days[i].high<<"   - "
+            <<arr_days[i].low<<"  - "<<arr_days[i].close<<" - "<<setw(10)<<arr_days[i].avg<<endl;
+    }
     return 0;
 }
